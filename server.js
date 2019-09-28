fbcf8f2b5f3f80e3f3c6f05a5a8794f6495c3391
Divan Visagie <visagiedivan@gmail.com>
2019-09-28 22:29:56 +0200

    Updates

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const app = express();
 
 const livereload = require('easy-livereload');
 
-app.use(express.static('public'))
+const app = express();
 
 app.use(livereload({
     app: app
   }));
+app.use(express.static('public'))
 
 app.listen(8080);
