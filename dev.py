fae8637baa60b70ef5b1d63e13fa3899c64d52fa
Divan Visagie <visagiedivan@gmail.com>
2020-04-18 21:11:51 +0200

    Fix breaking change in tornado

--- dev.py
+++ dev.py
@@ -2,6 +2,10 @@
 from src.builder import build
 from livereload import Server
 
+# Fix breaking change in tornado https://github.com/tornadoweb/tornado/issues/2608
+import asyncio
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 def main():
     # build()
     server = Server()
