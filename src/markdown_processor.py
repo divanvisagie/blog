7d2f58064e18e01bcd4d0b425576e6f99fdd7ddf
Divan Visagie <me@dvisagie.com>
2020-06-28 12:29:27 +0200

    Update markdown_processor.py

--- src/markdown_processor.py
+++ src/markdown_processor.py
@@ -4,4 +4,4 @@ from markdown.extensions.codehilite import CodeHiliteExtension as CodeHilite
 
 def markdown_to_html(html_string):
     cl = CodeHilite(linenums=False, use_pygments=True)
-    return markdown(html_string, extensions=[cl,'fenced_code','toc'])
+    return markdown(html_string, extensions=[cl,'fenced_code','toc','tables'])
