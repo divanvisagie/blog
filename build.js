f94cbb7a831230b19dfc5f2bc9e9950eed85bca9
Divan Visagie <me@dvisagie.com>
2020-10-02 23:39:14 +0000

    start node conversion

---
+++ build.js
@@ -0,0 +1,24 @@
+const { mkdir } = require('fs').promises
+const { existsSync } = require('fs')
+const { copy } = require('fs-extra')
+const { buildPages } = require('./src/builder')
+
+async function createBuildDir() {
+    const path = './build'
+    if (!existsSync(path)) {
+        await mkdir(path)
+    }
+}
+
+async function copyPublic() {
+    const startPath = './public'
+    const targetPath = './build'
+    await copy(startPath, targetPath)
+}
+
+async function main() {
+    await createBuildDir()
+    await copyPublic()
+    await buildPages()
+}
+main()
---
+++ src/builder.js
@@ -0,0 +1,6 @@
+async function buildPages() {
+    console.log('TODO: build the pages')
+}
+
+
+module.exports = { buildPages }
