710aed0f8d4632e8fb7233a1fc6ec437b9b5393d
Divan Visagie <me@dvisagie.com>
2020-10-31 12:37:04 +0200

    cleanup

--- dev.js
+++ dev.js
@@ -2,9 +2,19 @@ const connect = require('connect')
 const static = require('serve-static')
 const livereload = require('livereload')
 
+const PORT = 3000;
+
+
+
+
+/**
+ * Set up a server that will continuously update when the build directory updates
+ */
 const server = connect()
 server.use(static(__dirname + '/build'))
-server.listen(3000);
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
 
 var lrserver = livereload.createServer()
 lrserver.watch(__dirname + '/build')
