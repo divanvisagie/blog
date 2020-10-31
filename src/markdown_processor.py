51f37bb8fee082da3ab8875e7025db86e1b2f892
Divan Visagie <me@dvisagie.com>
2020-10-31 02:34:50 +0200

    Kill the snakes

--- src/markdown_processor.py
+++
@@ -1,7 +0,0 @@
-from markdown import *
-from markdown.extensions import Extension
-from markdown.extensions.codehilite import CodeHiliteExtension as CodeHilite
-
-def markdown_to_html(html_string):
-    cl = CodeHilite(linenums=False, use_pygments=True)
-    return markdown(html_string, extensions=[cl,'fenced_code','toc','tables'])
--- src/replacement_tags.py
+++
@@ -1,8 +0,0 @@
-CONTENT = '{{CONTENT}}'
-TITLE = '{{TITLE}}'
-DESCRIPTION = '{{DESCRIPTION}}'
-CARD_IMAGE = '{{CARD_IMAGE}}'
-
-ROOT = '.'
-TEMPLATE_ROOT = './templates'
-OUT_DIR = 'build'
