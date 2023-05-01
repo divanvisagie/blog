84fa734c5c8d6915270074d1dbfd317eafd95aa3
Divan Visagie <me@divanv.com>
2023-05-01 16:28:44 +0200

    remove semicolons

--- build.ts
+++ build.ts
@@ -1,27 +1,27 @@
 // const { mkdir } = require("fs").promises;
-import { existsSync } from "fs";
-import { copy } from "fs-extra";
-import { mkdir } from "fs/promises";
-import { buildPages } from "./src/builder";
+import { existsSync } from 'fs'
+import { copy } from 'fs-extra'
+import { mkdir } from 'fs/promises'
+import { buildPages } from './src/builder'
 
 async function createBuildDir() {
-  const path = "./build";
+  const path = './build'
   if (!existsSync(path)) {
-    await mkdir(path);
+    await mkdir(path)
   }
 }
 
 async function copyPublic() {
-  const startPath = "./public";
-  const targetPath = "./build";
-  await copy(startPath, targetPath);
+  const startPath = './public'
+  const targetPath = './build'
+  await copy(startPath, targetPath)
 }
 
 export async function build() {
-  console.log("Building website...");
-  await createBuildDir();
-  await copyPublic();
-  await buildPages();
+  console.log('Building website...')
+  await createBuildDir()
+  await copyPublic()
+  await buildPages()
 }
 
-build();
+build()
